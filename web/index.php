1b0eb6dfb59098b116d6d1e3ef660ed7eaa76824
Your Name <you@example.com>
2018-12-01 21:14:07 +0530

    first commit

---
+++ web/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+       <title>InstaDownloader</title>
+<meta name="theme-color" content="#37474F">
+        <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<link rel="stylesheet" href="style.css">
+<script src="script.js"></script>
+    </head>
+    <body>
+     <div class="container">
+        <div class="header"><h3><u>Insta<span>Downloader</span></u></h3></div>
+        <form onsubmit="return false">
+        <input class="form-control search" id="url"   placeholder="Instagram post URL">
+        <button class="btn btn-success" id="check">Check for download</button><br>
+        <center>
+        <a href="" download target="blank" id="btnd" class="btn"></a></center>
+        <h4 id="title"></h4><br>
+        <center>
+        <img width="90%" src="" id="img"><br>
+        </center>
+        </form>
+             <span class="cp">copyright &copy; Nikky Amresh </span>
+        </div>
+    </body>
+</html>
---
+++ web/script.js
@@ -0,0 +1,39 @@
+$(function(){
+function check(url){
+function read(e){
+    var ic,au;
+    var r=(new DOMParser).parseFromString(e,"text/html").querySelector("html");
+    var metas = r.getElementsByTagName('meta'); 
+for (var i=0; i<metas.length; i++) { 
+if (metas[i].getAttribute("property") == "og:image") { $("#img").attr("src",metas[i].getAttribute("content"));$(".container").css("height",580) ;}
+if (metas[i].getAttribute("property") == "og:title") { $("#title").html(metas[i].getAttribute("content"));}
+if (metas[i].getAttribute("property") == "og:video"||metas[i].getAttribute("property") == "og:image"){ $("#btnd").attr("href",metas[i].getAttribute("content")); $("#btnd").html("Download"); $("#check").html("Check for download"); $("#check").removeAttr("disabled");
+$("#btnd").addClass("btn-success")
+$("#btnd").removeClass("btn-danger")
+break;
+
+} }return "";
+   }     var t=new XMLHttpRequest;
+        t.onreadystatechange=function(){ if(t.readyState==XMLHttpRequest.DONE&&200==t.status){var n=read(t.responseText);}else{
+        if(t.readyState==XMLHttpRequest.DONE){
+        $("#btnd").html("INVALID URL")
+        $("#btnd").removeAttr("href")
+        $("#title").html("")
+        $("#btnd").addClass("btn-danger")
+        $("#btnd").removeClass("btn-success")
+        $("#img").removeAttr("src")
+        $("#check").html("Check for download"); $("#check").removeAttr("disabled");}
+}};
+         var n="https://cors-anywhere.herokuapp.com/"+url;
+        t.open("GET",n,!0),
+        t.send(null)
+        }
+        $("#check").click(function(){
+          if($("#url").val().trim()!=""){
+            $("#check").attr("disabled",true);
+            $("#check").html("Processing...");
+            check($("#url").val());}else{
+                $("#url").focus();
+            }
+        })
+  })
